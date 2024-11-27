a4a5b8876729998fd30d569c1a0b6ffb2cdbc1fe
Hassan-1995 <63869278+Hassan-1995@users.noreply.github.com>
2024-11-27 08:33:35 +0500

    Update Issues

--- app/issues/...components/IssueForm.tsx
+++ app/issues/...components/IssueForm.tsx
@@ -87,7 +87,7 @@ const IssueForm = ({ issue }: Props) => {
           <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
           <Button disabled={isSubmitting}>
-            {issue ? "Update Issue" : "Submit New Issue"}
+            {issue ? "Update Issue" : "Submit New Issue"}{" "}
             {isSubmitting && <Spinner />}
           </Button>
         </form>
